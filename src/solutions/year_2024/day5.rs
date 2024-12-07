e401fa3ae8b21fa9f2d1e87ed37ea045062936c4
koji.lin <koji.lin@lycorp.co.jp>
2024-12-07 09:33:06 +0900

    Solve day5 2nd

--- src/solutions/year_2024/day5.rs
+++ src/solutions/year_2024/day5.rs
@@ -50,19 +50,63 @@ fn solve_day5() -> Result<i64, Box<dyn Error>> {
     Ok(total)
 }
 
-fn dfs(current: i64, adjacents: &HashMap<i64, Vec<i64>>, visited: &mut HashSet<i64>, queue: &mut Vec<i64>) {
-    if visited.contains(&current) {
-        return;
-    }
-    visited.insert(current);;
-    adjacents.get(&current).unwrap_or(&vec![]).iter().for_each(|&x| {
-        dfs(x, adjacents, visited, queue);
-    });
-    queue.insert(0, current);
-}
-
 fn solve_day5_2() -> Result<i64, Box<dyn Error>> {
-    Ok(0)
+    let input = fs::read_to_string("src/solutions/year_2024/day5.txt")?;
+    let lines: Vec<&str> = input.lines().collect();
+    let mut total = 0i64;
+    let mut is_checking = false;
+    let mut rules: HashMap<i64, HashSet<i64>> = HashMap::new();
+
+    for line in lines {
+        if line.is_empty() {
+            is_checking = true;
+            continue;
+        }
+        if is_checking {
+            // logic
+            let mut target: Vec<i64> = line.split(",").map(|c| c.parse::<i64>().unwrap()).collect();
+            let mut current_index = 0usize;
+            let mut contains_fail_order = false;
+            while current_index < target.len() {
+                let mut has_wrong = -1;
+                for j in current_index + 1..target.len() {
+                    if rules.get(&(target[current_index])).unwrap_or(&HashSet::new()).contains(&target[j]) {
+                        has_wrong = j as i64;
+                        continue;
+                    }
+                }
+                // put to the last
+                if has_wrong != -1 {
+                    contains_fail_order = true;
+                    let v = target.remove(current_index);
+                    target.insert(has_wrong as usize, v);
+                    continue;
+                }
+                current_index += 1;
+            }
+
+            if contains_fail_order {
+                println!("{:?}", target);
+                total += target[target.len() / 2];
+            } else {
+                println!("no change: {:?}", target);
+            }
+        } else {
+            let nums: Vec<i64> = line.split("|")
+                .map(|content| content.parse::<i64>().unwrap()).collect();
+
+            rules.entry(nums[1])
+                .and_modify(|e| {
+                    e.insert(nums[0]);
+                })
+                .or_insert_with(|| {
+                    let mut set = HashSet::new();
+                    set.insert(nums[0]);
+                    set
+                });
+        }
+    }
+    Ok(total)
 }
 
 #[cfg(test)]
@@ -73,6 +117,13 @@ mod test {
     fn test_day5_1() -> Result<(), Box<dyn Error>> {
         println!("-----real-----");
         println!("result:{}", solve_day5()?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_day5_2() -> Result<(), Box<dyn Error>> {
+        println!("-----real-----");
+        println!("result:{}", solve_day5_2()?);
         Ok(())
     }
 }
