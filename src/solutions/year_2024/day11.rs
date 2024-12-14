5c3dbe176784280f9896b48cd58ca01f06591366
koji.lin <koji.lin@gmail.com>
2024-12-14 10:25:36 +0900

    day 11

--- src/solutions/year_2024/day11.rs
+++ src/solutions/year_2024/day11.rs
@@ -2,82 +2,63 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::fs;
 
-fn solve_day11() -> Result<i64, Box<dyn Error>> {
+fn solve_day11() -> Result<usize, Box<dyn Error>> {
     let input = fs::read_to_string("src/solutions/year_2024/day11.txt")?;
     let mut nums: Vec<i64> = input
-        .split(" ")
-        .map(|x| (x).parse::<i64>().unwrap())
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
         .collect();
 
     for _ in 0..25 {
-        let mut new_nums: Vec<i64> = Vec::new();
-        for &str in nums.iter() {
-            let to_str = str.to_string();
-            if str == 0 {
-                new_nums.push(1);
-            } else if to_str.len() % 2 == 0 {
-                new_nums.push(to_str[0..to_str.len() / 2].parse::<i64>().unwrap());
-                new_nums.push(
-                    (&to_str[to_str.len() / 2..to_str.len()])
-                        .parse::<i64>()
-                        .unwrap(),
-                );
-            } else {
-                new_nums.push(str * 2024);
-            }
-        }
-        nums = new_nums;
+        nums = nums
+            .into_iter()
+            .flat_map(|num| {
+                let num_str = num.to_string();
+                if num == 0 {
+                    vec![1]
+                } else if num_str.len() % 2 == 0 {
+                    let (left, right) = num_str.split_at(num_str.len() / 2);
+                    vec![left.parse().unwrap(), right.parse().unwrap()]
+                } else {
+                    vec![num * 2024]
+                }
+            })
+            .collect();
     }
 
-    Ok(nums.len() as i64)
+    Ok(nums.len())
 }
 
 fn solve_day11_2() -> Result<i64, Box<dyn Error>> {
     let input = fs::read_to_string("src/solutions/year_2024/day11.txt")?;
     let nums: Vec<i64> = input
-        .split(" ")
-        .map(|x| (x).parse::<i64>().unwrap())
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
         .collect();
 
-    let mut result = 0;
     let mut cache = HashMap::new();
-    for num in nums {
-        result += dp(num, 75, &mut cache);
-    }
-
-    Ok(result)
+    Ok(nums.into_iter().map(|num| dp(num, 75, &mut cache)).sum())
 }
 
-fn dp(num: i64, round: i32, cache: &mut HashMap<String, i64>) -> i64 {
+fn dp(num: i64, round: i32, cache: &mut HashMap<(i64, i32), i64>) -> i64 {
     if round == 0 {
         return 1;
     }
-    let key = &(num.to_string() + "_" + &round.to_string());
-    if cache.contains_key(key) {
-        return *cache.get(key).unwrap();
+    let key = (num, round);
+    if let Some(&result) = cache.get(&key) {
+        return result;
     }
-    let mut result = 0;
-    let to_str = num.to_string();
-    if num == 0 {
-        result = dp(1, round - 1, cache);
-    } else if to_str.len() % 2 == 0 {
-        result += dp(
-            to_str[0..to_str.len() / 2].parse::<i64>().unwrap(),
-            round - 1,
-            cache,
-        );
-        result += dp(
-            (to_str[to_str.len() / 2..to_str.len()])
-                .parse::<i64>()
-                .unwrap(),
-            round - 1,
-            cache,
-        );
+    let result = if num == 0 {
+        dp(1, round - 1, cache)
+    } else if num.to_string().len() % 2 == 0 {
+        let num_str = num.to_string();
+        let (left, right) = num_str.split_at(num_str.len() / 2);
+        dp(left.parse().unwrap(), round - 1, cache) + dp(right.parse().unwrap(), round - 1, cache)
     } else {
-        result = dp(num * 2024, round - 1, cache);
-    }
+        dp(num * 2024, round - 1, cache)
+    };
 
-    cache.insert(key.to_string(), result);
+    cache.insert(key, result);
     result
 }
 
